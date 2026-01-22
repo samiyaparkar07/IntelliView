c7c105e8c00a5ac32ed3c2fe6c51c63ee347da57
samiyaparkar07 <parkarsamiya1008@gmail.com>
2026-01-22 22:09:19 +0530

    remove stream

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -22,11 +22,7 @@ const syncUser = inngest.createFunction(
 
     await User.create(newUser);
 
-    await upsertStreamUser({
-      id: newUser.clerkId.toString(),
-      name: newUser.name,
-      image: newUser.profileImage,
-    });
+
   }
 );
 
@@ -37,7 +33,7 @@ const deleteUserFromDB = inngest.createFunction(
     await connectDB();
 
     const { id } = event.data;
-    await User.deleteOne({ clerkId: id });
+
 
     await deleteStreamUser(id.toString());
   }
