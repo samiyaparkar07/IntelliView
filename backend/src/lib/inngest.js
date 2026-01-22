786c14b578a3ed9db96b2cd1a872314573367f51
samiyaparkar07 <parkarsamiya1008@gmail.com>
2026-01-22 23:16:55 +0530

    update

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -3,7 +3,7 @@ import { connectDB } from "./db.js";
 import User from "../models/User.js";
 
 
-export const inngest = new Inngest({ id: "talent-iq" });
+export const inngest = new Inngest({ id: "intelliView" });
 
 const syncUser = inngest.createFunction(
   { id: "sync-user" },
